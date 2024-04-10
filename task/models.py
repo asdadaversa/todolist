b67ebb78ad0a5dd608273cf85f25d6daee2271ee
asdadaversa <peikqudakeus@gmail.com>
2024-04-10 16:31:50 +0300

    create Task and Tags model, register in admin, run migrations

--- task/models.py
+++ task/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"tag: {self.name}"
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    task_done = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
+
+    class Meta:
+        ordering = ("datetime", )
+
+    def __str__(self):
+        return f"content: {self.content}"
