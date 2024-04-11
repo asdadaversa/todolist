daa2110870dcc8ca9c8343362046c612eda6dcdc
asdadaversa <peikqudakeus@gmail.com>
2024-04-11 16:46:07 +0300

    update models, run migrations

--- task/models.py
+++ task/models.py
@@ -10,13 +10,13 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.TextField()
-    datetime = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     task_done = models.BooleanField()
     tags = models.ManyToManyField(Tag, related_name="tasks", blank=True)
 
     class Meta:
-        ordering = ("datetime", )
+        ordering = ("created", )
 
     def __str__(self):
         return self.content
