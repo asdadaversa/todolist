5f7d0efa7884c53e1087f9be66aaebcaef205ff0
asdadaversa <peikqudakeus@gmail.com>
2024-04-11 18:53:56 +0300

    create functions for complete and undo tasks

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from task.views import TaskListView, tags, TaskCreateView, TagCreateView, TaskUpdateView, TagUpdateView, TaskDeleteView, \
-    TagDeleteView
+    TagDeleteView, task_complete, task_undo
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
@@ -9,10 +9,14 @@ urlpatterns = [
     path("create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/complete", task_complete, name="task-complete"),
+    path("task/<int:pk>/undo", task_undo, name="task-undo"),
 
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+
+
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -44,7 +44,7 @@ class TaskCreateView(generic.CreateView):
 class TagCreateView(generic.CreateView):
     model = Tag
     template_name = 'task/tag_form.html'
-    success_url = reverse_lazy("tag:index")
+    success_url = reverse_lazy("task:tags")
     form_class = TagCreateForm
 
 
@@ -57,7 +57,7 @@ class TaskUpdateView(generic.UpdateView):
 class TagUpdateView(generic.UpdateView):
     model = Tag
     form_class = TagCreateForm
-    success_url = reverse_lazy("tag:index")
+    success_url = reverse_lazy("task:tags")
 
 
 class TaskDeleteView(generic.DeleteView):
@@ -67,4 +67,43 @@ class TaskDeleteView(generic.DeleteView):
 
 class TagDeleteView(generic.DeleteView):
     model = Tag
-    success_url = reverse_lazy("tag:index")
+    success_url = reverse_lazy("task:tags")
+
+
+def task_complete(request: HttpRequest, pk: int) -> HttpResponse:
+    task = get_object_or_404(Task, pk=pk)
+    if task.task_done is False:
+        task.task_done = True
+        task.save()
+    return redirect("task:index")
+
+
+def task_undo(request: HttpRequest, pk: int) -> HttpResponse:
+    task = get_object_or_404(Task, pk=pk)
+    if task.task_done is True:
+        task.task_done = False
+        task.save()
+    return redirect("task:index")
+
+# def add_comment(request: HttpRequest, pk: int) -> HttpResponse:
+#     if request.method == "POST":
+#         user = request.user
+#         if not user.is_authenticated:
+#             raise ValidationError("User has to be authenticated!")
+#         post_id = request.POST.get("post_id")
+#         content = request.POST.get("content")
+#         post = Post.objects.get(pk=post_id)
+#         Commentary.objects.create(
+#             user=user,
+#             post=post,
+#             content=content,
+#         )
+#         return redirect("blog:post-detail", pk=post_id)
+#
+# @login_required()
+# def car_delete_driver(
+#         request: HttpRequest,
+#         pk: int,
+# ) -> HttpResponse:
+#     Car.objects.get(id=pk).drivers.remove(request.user)
+#     return redirect("taxi:car-detail", pk)
